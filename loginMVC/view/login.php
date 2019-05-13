0a5037d746e5972201ad2d84fa2cb6679b0eee82
cristearares24 <48632989+cristearares24@users.noreply.github.com>
2019-05-13 20:53:18 +0300

    Add files via upload

---
+++ loginMVC/view/login.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Academic Thesis Manager</title>
+    <link rel="stylesheet" href="Style/css.css">
+  </head>
+
+<body>
+
+<nav class="antet">   
+            
+            <img id="fii" src="Resources/fiialb.png" alt="FII">
+                
+            <div id="titlu">Academic Thesis Manager</div>
+
+            <img id="univ" src="Resources/UNIV1.png" alt="UNIV">
+    </nav>
+<!-- <?php 
+ echo $reslt;
+
+?> -->
+<form action="" method="POST">
+<div class="container">
+              <div id="name">Username</div>
+              <input id="username" value="" name="username" type="text" required="required" placeholder="Username"><br>
+
+              <div id="pass">Password</div>
+              <input id="password" name="password" type="password" required="required" placeholder="Password" />
+          
+              <button type="submit" class="green big" name="submit"><span>Login</span></button>
+              <label>
+                  <input type="checkbox" checked="checked" name="remember"> Remember me
+              </label>
+        </div>
+ </form>
+
+</body>
+</html>
+
+
+
+
