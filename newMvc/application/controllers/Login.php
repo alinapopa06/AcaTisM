22e81d1a7c89ce5cd858441fd3958e7e1167c7bf
stefanbolo17 <stefan_bolota17@yahoo.com>
2019-05-20 18:15:41 +0300

    Update Login.php

--- newMvc/application/controllers/Login.php
+++ newMvc/application/controllers/Login.php
@@ -7,18 +7,10 @@ class Login extends Controller {
 	function index()
 	{
 		$model=new Login_model();
-		$rez=$model->getSomething(1);
 		$template = $this->loadView('login_view');
 		$template->render();
-		$sesiune=new Session_helper();
 	}
-	function newMethod()
-	{
-		$template=$this->loadView('newView');
-		$template->set("nume","Popa");
-		$template->set("prenume","ALina");
-		$template->render();
-	}
+
 }
 
 ?>
