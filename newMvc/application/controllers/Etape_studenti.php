057ed3e00be475f6d24d8edafc98fc897eeaf9ad
malinaelena98 <d.malinaelena98@yahoo.ro>
2019-06-13 21:36:04 +0300

    Commit bun

--- newMvc/application/controllers/Etape_studenti.php
+++ newMvc/application/controllers/Etape_studenti.php
@@ -5,11 +5,21 @@ class Etape_studenti extends Controller {
 	function index()
 	{
 		$help=$this->loadHelper('session_helper');
-		if($help->get("id_studenst")!=NULL)
+		if($help->get("stud")!=NULL)
 		{
+			$model = $this->loadModel('etape_studenti_model');
+			$model->setStud($help->get("stud"));
 			$template = $this->loadView('etape_stud_view');
+			$template->set("studInfo",$model->getStudInfo());
+			$template->set("studID",$model->getInfo());
+			$template->set('etape',$model->getEtape());
+			$template->set('proiecte',$model->getProject());
+			$template->set('etape_arr',$model->getDescriereEtapa());
+
+
+
+
 			$template->render();
-			$help->destroy();
 		}
 		else
 		{
--- newMvc/application/views/etape_stud_view.php
+++ newMvc/application/views/etape_stud_view.php
@@ -1,115 +1,122 @@
+<?php
+$studInfo = $studInfo[0];
+//print_r($etape);
+//$etape = $etape[0];
+if(count($proiecte)>0) {$proiecte = $proiecte[0];
+     $etape_arr=$etape_arr[0]['descriere'];
+$etape_arr=explode('.',$etape_arr);
+}
+
+?>
 <!DOCTYPE html>
 <html lang="ro">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>static/css/StefanStyle_studenti.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Etape</title>
-  </head>
-  <body>
-   <nav class="menu">   
-          <div id="siglafii">
-            <img id="fii" src="<?php echo BASE_URL; ?>static/images/fiialb.png" alt="fii">
-          </div>
-                 <a href="acatism" >Acasa</a>
-                <a href="proiecte_studenti" >Proiecte</a>
-                <a href="Etape_start" class="active" >Etape</a>
-                <a href="Contact" >Contact</a>
-                <a href="<?php echo BASE_URL."Logout"; ?>">Logout</a>
-           <div id="siglauniv">
-            <img id="univ" src="<?php echo BASE_URL; ?>static/images/UNIV1.png" alt="universitate">
-          </div>
-    </nav>
-     <div class="content">
-        <div class="jumbotron">
-            <h1>Profilul meu</h1>
-            <p class="textdeco">Licenta</p>
-            <p class="detalii"><span class="textdeco">An de studiu:</span> II </p>
-            <p class="detalii"><span class="textdeco">Grupa:</span> A3</p>
-          </div>
-        </div>
-        <h1>Etapele mele</h1>
-   <div class="grid-container">
-      <div class="Etapa1 active_project">
-        <h1>Depistarea bibliografiei-schitarea planului</h1>
-        <ol>
-            <li>cursul cu bibliografia orientativa</li>
-            <li>profesorul conducator si rolul sau</li>
-            <li> investigatia personala</li>
-        </ol>
-          <select class="dropdown1">
-			<option value="0">Finalizata</option>
-			<option value="1">In progres</option>
-			<option value="2">Neinceput</option>
-		  </select>
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>static/css/StefanStyle_studenti.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Etape</title>
+</head>
+
+<body>
+  <nav class="menu">
+    <div id="siglafii">
+      <img id="fii" src="<?php echo BASE_URL; ?>static/images/fiialb.png" alt="fii">
+    </div>
+    <a href="acatism">Acasa</a>
+    <a href="proiecte_studenti">Proiecte</a>
+    <a href="Etape_studenti" class="active">Etape</a>
+    <a href="Contact">Contact</a>
+    <a href="<?php echo BASE_URL . "Logout"; ?>">Logout</a>
+    <div id="siglauniv">
+      <img id="univ" src="<?php echo BASE_URL; ?>static/images/UNIV1.png" alt="universitate">
+    </div>
+  </nav>
+  <div class="content">
+    <div class="jumbotron">
+      <h1>Profilul meu: <?= $studInfo['nume_student'] ?> <?= $studInfo['prenume_student'] ?></h1>
+      <p class="textdeco"><?= ucfirst($studInfo['tip']) ?></p>
+      <p class="detalii"><span class="textdeco">An de studiu:</span> <?= $studInfo['an'] ?> </p>
+      <p class="detalii"><span class="textdeco">Grupa:</span> <?=$studInfo['grupa']?></p>
+      <?php
+          if(count($proiecte)>0) 
+          {
+            echo '<p class="detalii"><span class="textdeco">Proiect:</span>' . $proiecte['nume_proiect'] . '</p>';
+            
+          }
+          else
+          echo '<p class="detalii"><span class="textdeco">Proiect:</span>Niciun proiect</p>';
+
+          
+
+
+        ?>
+  
+
+    </div>
+  </div>
+  <h1>Etapele mele</h1>  
+  <div class="etape">
+  <?php
+//print_r($etape);
+for($i=0;$i<count($etape);$i++){
+  if($i%2==0)
+  {
+echo '
+  <div class="row">
+    <div class="column">
+      <div class="Etapa1 '.$etape[$i]['status'].'">
+       <h1> '.  $etape[$i]["nume_etapa"] .'</h1>
+        <ol>';
+
+       // $etape_arr=$etape_arr[0]['descriere'];
+        $etape_arr=explode('.',$etape[$i]['descriere']);
+
+           foreach($etape_arr as $descriere){
+            echo '<li>' . $descriere . '</li>' ;
+           }
+        
+     echo ' </ol>
       </div>
-      <div class="Etapa2 active_project"> 
-       <h1>Informarea si documentarea</h1>
-        <ol>
-            <li> întocmirea fisierului propriu de documentare</li>
-            <li>clasificarea informatiei</li>
-            <li> ierarhizarea informatiei</li>
-            <li>    încadrarea în context</li>
-            <li>formarea unei competente informational-documentare</li>
-        </ol>
-          <select class="dropdown1">
-            <option value="0">Finalizata</option>
-            <option value="1">In progres</option>
-            <option value="2">Neinceput</option>
-         </select>
+    </div>
+    ';
+  }
+  else
+  {
+    echo '
+    <div class="column">
+    <div class="Etapa1 '.$etape[$i]['status'].'">
+      <h1> '.  $etape[$i]["nume_etapa"] .'</h1>
+      <ol>';
+
+      // $etape_arr=$etape_arr[0]['descriere'];
+       $etape_arr=explode('.',$etape[$i]['descriere']);
+
+          foreach($etape_arr as $descriere){
+           echo '<li>' . $descriere . '</li>' ;
+          }
+       
+    echo ' </ol>
       </div>
-      <div class="Etapa3 work_in_progress">
-       <h1> Intocmirea planului detaliat al lucrarii </h1>
-        <ol>
-            <li>planul se face într-o faza avansata a informarii si documentarii</li>
-            <li>cautarea unui model existent</li>
-            <li>stabilirea structurii generale a planului (introducere, cuprins/tratare, concluzii/încheiere)</li>
-            <li>examinarea critica a datelor cunoscute</li>
-        </ol>
-           <select class="dropdown1">
-            <option value="0">Finalizata</option>
-            <option value="1">In progres</option>
-            <option value="2">Neinceput</option>
-         </select>
     </div>
-      <div class="Etapa4 not_working">
-       <h1>Elaborarea primei forme a lucrarii</h1>
-       <ol>
-       		<li>alegerea metodelor de lucru si formularea lor</li>
-       		<li>redactarea propriu-zisa în prima forma</li>
-       		<li>dezvoltarea aplicatiei</li>
-       </ol>
-       <select class="dropdown1">
-        <option value="0">Finalizata</option>
-        <option value="1">In progres</option>
-        <option value="2">Neinceput</option>
-      </select>
     </div>
-     <div class="Etapa5 not_working">
-       <h1>Elaborarea finala a întregii lucrari</h1>
-       <ol>
-       		<li>întocmirea foii de titlu</li>
-       		<li>cuprinsul</li>
-       		<li>prezentarea externa</li>
-       </ol>
-       <select class="dropdown1">
-        <option value="0" class="drop">Finalizata</option>
-        <option value="1">In progres</option>
-        <option value="2">Neinceput</option>
-      </select>
-    </div>
-     </div>
-    <div class="container">
-        <h1>Progresul proiectului</h1>
-          <ul class="progressbar">
-          	 <li class="active">Schitarea planulu</li>
-            <li class="active">Documentare</li>
-            <li class="working">Intocmirea planului</li>
-            <li>Elaborarea initiala</li>
-             <li>Elaborarea finala</li>
-          </ul>
-    </div>
-	<h1>Editare in git</h1>
-	<img src="<?php echo BASE_URL; ?>static/images/git.png" id="img" alt="git">
-  </body>
-</html>
+    ';
+  }
+  
+}
+    ?>
+  </div>
+</div>
+  <div class="container">
+    <h1>Progresul proiectului</h1>
+    <ul class="progressbar">
+      <li class="active">Schitarea planulu</li>
+      <li class="active">Documentare</li>
+      <li class="working">Intocmirea planului</li>
+      <li>Elaborarea initiala</li>
+      <li>Elaborarea finala</li>
+    </ul>
+  </div>
+  
+</body>
+
